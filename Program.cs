e2e51d26fba8f206082fa6a2e7d7d0d904cd157d
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-04 20:04:20 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp4.models;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Sound();
+
+            Dog dog = new Dog();
+            dog.Sound();
+            
+            Cat cat = new();
+            cat.Sound();
+            
+            Duck duck = new();
+            duck.Sound();
+            
+        }
+    }
+}
---
+++ models/Duck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.models
+{
+    internal class Duck : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Quack Quack ......");
+        }
+    }
+}
